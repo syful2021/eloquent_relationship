677d4415ad9b5235f4f3734a561b3470c60d46e7
syful2021 <mdsyfulislam20009@gmail.com>
2024-09-28 15:34:26 +0600

    Faker use for DB seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Faker\Factory as Faker;
 
 class UserSeeder extends Seeder
 {
@@ -13,13 +14,15 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        
-
-        $user = new User;
-        $user->name = "User One";
-        $user->email = "userone@gmail.com";
-        $user->password = "12345678";
-        $user->save();
+        $faker = Faker::create();       // Faker create random data for insert on DB table;
+        for( $i=1; $i <=5; $i++){
+            $user = new User;
+            $user->name = $faker->name;
+            $user->email = $faker->email;
+            $user->password = $faker->password;
+            $user->save();
+        }
+       
 
     }
 }
